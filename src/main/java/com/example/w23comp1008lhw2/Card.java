7a5b69956f62448064a2d44f535b778b64595691
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-18 09:27:27 -0500

    Added the Main class

--- src/main/java/com/example/w23comp1008lhw2/Card.java
+++ src/main/java/com/example/w23comp1008lhw2/Card.java
@@ -1,4 +1,18 @@
 package com.example.w23comp1008lhw2;
 
 public class Card {
+    private String suit;
+    private String faceName;
+
+    /**
+     * This is the constructor, it will create a new Card object in system
+     * memory.  It will validate the suit and faceName are found in standard
+     * playing cards
+     * @param suit "hearts","clubs","spades","diamonds"
+     * @param faceName "2","3","4",..."jack","queen","king","ace"
+     */
+    public Card(String suit, String faceName) {
+        this.suit = suit;
+        this.faceName = faceName;
+    }
 }
---
+++ src/main/java/com/example/w23comp1008lhw2/Main.java
@@ -0,0 +1,7 @@
+package com.example.w23comp1008lhw2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
