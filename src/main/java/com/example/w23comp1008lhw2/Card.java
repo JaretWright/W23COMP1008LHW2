a68117b86c586dd39289179bcd1ff39a93ac49b3
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-18 09:47:41 -0500

    Added validation for the setSuit method.

--- src/main/java/com/example/w23comp1008lhw2/Card.java
+++ src/main/java/com/example/w23comp1008lhw2/Card.java
@@ -1,5 +1,8 @@
 package com.example.w23comp1008lhw2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Card {
     private String suit;
     private String faceName;
@@ -12,7 +15,40 @@ public class Card {
      * @param faceName "2","3","4",..."jack","queen","king","ace"
      */
     public Card(String suit, String faceName) {
-        this.suit = suit;
+        setSuit(suit);
         this.faceName = faceName;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * Validates the argument and sets the instance variable
+     * @param suit "hearts","clubs","spades","diamonds"
+     */
+    public void setSuit(String suit) {
+        List<String> validSuits = Arrays.asList("hearts","clubs","spades","diamonds");
+
+        //test if the argument is in the list of valid suits
+        if (validSuits.contains(suit))
+            this.suit = suit;
+        else
+            throw new IllegalArgumentException(suit + " was not in the list of "+
+                    validSuits);
+    }
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    public void setFaceName(String faceName) {
+        this.faceName = faceName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return faceName + " of " + suit;
     }
 }
--- src/main/java/com/example/w23comp1008lhw2/Main.java
+++ src/main/java/com/example/w23comp1008lhw2/Main.java
@@ -2,6 +2,7 @@ package com.example.w23comp1008lhw2;
 
 public class Main {
     public static void main(String[] args) {
-
+        Card card1 = new Card("coffee","2");
+        System.out.println(card1);
     }
 }
